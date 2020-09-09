2d0be060fce6a2288d11c0d24d717eca655c4d06
Megan Leich <51799170+maleich@users.noreply.github.com>
2020-09-08 19:28:45 -0700

    Write code for testing

--- my_code.py
+++ my_code.py
@@ -1,15 +1,18 @@
 # Collaborators (including web sites where you got help: (enter none if you didn't need help)
 #  
 
-def inc(x):
-    # User code goes here
-    return x + 1
+def factorial_calc(x):
+    fac = 1
+    for i in range(x):
+        i += 1
+        fac = fac * i # User code goes here
+    return fac
 
 
 if __name__ == '__main__':
     # Test your code with this first
     # Change the argument to try different values
-    print(inc(6))
+    print(factorial_calc(5))
 
     # After you are satisfied with your results, use input() to prompt the user for a value:
     #v = input("Value to increment: ")
